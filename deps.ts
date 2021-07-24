bb5343ac4cb34ff7b96cc105a8b194a863db3bd0
RakinKhan <rakinalikhan@gmail.com>
2021-07-24 12:43:42 -0400

    adding deps.ts file with just the react ESM dependencies

---
+++ deps.ts
@@ -0,0 +1,6 @@
+// React and Related Dependencies
+import React from "https://esm.sh/react?dts";
+import ReactDOM from "https://esm.sh/react-dom?dts";
+import ReactDOMServer from "https://esm.sh/react-dom/server?dts";
+
+export { React, ReactDOM, ReactDOMServer };
